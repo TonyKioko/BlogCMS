47e957aed6a5b7e9df9e354bf4aa288b5447e4ca
TonyKioko <tonykioko384@gmail.com>
2019-04-25 12:33:30 +0300

    category query builder changed

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -16,7 +16,7 @@ class FrontEndController extends Controller
 {
     public function index(){
 
-        return view('index')->with('title',Setting::first()->site_name)->with('categories',Category::take(4)->get())->with('first_post',Post::orderBy('created_at','desc')->first())->with('second_post',Post::orderBy('created_at','desc')->skip(1)->take(1)->get()->first())->with('third_post',Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first())->with('Laravel',Category::find(2))->with('Wordpress',Category::find(1))->with('settings',Setting::first());
+        return view('index')->with('title',Setting::first()->site_name)->with('categories',Category::take(4)->get())->with('first_post',Post::orderBy('created_at','desc')->first())->with('second_post',Post::orderBy('created_at','desc')->skip(1)->take(1)->get()->first())->with('third_post',Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first())->with('Laravel',Category::find(4))->with('Wordpress',Category::find(5))->with('settings',Setting::first());
     }
 
 
